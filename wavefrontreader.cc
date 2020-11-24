f0efb98e4bb1359c4c6252c8e0f36d304755c97c
Anders Olme <anders.olme@gmail.com>
2020-11-24 21:24:53 +0100

    Vertex struct populated
    now hopefully correctly populated.

--- wavefrontreader.cc
+++ wavefrontreader.cc
@@ -15,6 +15,9 @@ void WaveFrontReader::readVertices(Mesh& obj) {
     uint32_t vector_order{
         0}; // we use this to error out if order is changed in .obj file
     uint32_t vector_index{0};
+    std::vector<glm::vec3> vertices;
+    std::vector<glm::vec2> textureCoords;
+    std::vector<glm::vec3> normals;
     if (myfile.is_open()) {
         while (getline(myfile, line)) {
             auto vpos = line.find("v ");
@@ -25,39 +28,28 @@ void WaveFrontReader::readVertices(Mesh& obj) {
                 conv >> x >> y >> z;
                 std::cout << "v x " << x << " y " << y << " z " << z
                           << std::endl;
-                Vertex vert;
-                vert.Coord = glm::vec3(x, y, z);
-                obj.vertices.push_back(vert);
+                vertices.push_back(glm::vec3(x,y,z));
                 continue;
             }
             vpos = line.find("vt ");
             if (vpos == 0) {
-                if (vector_order == 1) {
-                    vector_index = 0;
-                }
-                vector_order = 2;
-
-                std::stringstream conv{line.substr(2)};
+                              std::stringstream conv{line.substr(2)};
                 float x, y;
                 conv >> x >> y;
                 std::cout << "vt x " << x << " y " << y << std::endl;
-                obj.vertices[vector_index].TextureCoord = glm::vec2(x, y);
-                vector_index++;
+                textureCoords.push_back(glm::vec2(x, y));
                 continue;
             }
             vpos = line.find("vn ");
             if (vpos == 0) {
-                if (vector_order == 2) {
-                    vector_index = 0;
-                }
                 vector_order = 3;
 
                 std::stringstream conv{line.substr(2)};
                 float x, y, z;
                 conv >> x >> y >> z;
                 std::cout << "vn x " << x << " y " << y << std::endl;
-                obj.vertices[vector_index].Normal = glm::vec3(x, y, z);
-                vector_index++;
+                normals.push_back(glm::vec3(x, y, z));
+            
                 continue;
             }
             vpos = line.find("f ");
@@ -70,10 +62,12 @@ void WaveFrontReader::readVertices(Mesh& obj) {
                 std::string face_index;
                 uint64_t index;
                 while (std::getline(splitter, face, ' ')) {
+                    Vertex vert;
                     std::cout << face << std::endl;
                     auto start_pos = face.find('/');
                     index = std::stoul(face.substr(0, start_pos), nullptr);
                     obj.indicies.push_back((uint32_t)index - 1);
+                    vert.Coord=vertices[index-1];
                     // std::cout << "start_pos" << start_pos << std::endl;
                     auto end_pos = face.find('/', start_pos + 1);
                     // std::cout << "end_pos" << end_pos << std::endl;
@@ -83,11 +77,14 @@ void WaveFrontReader::readVertices(Mesh& obj) {
                                                    end_pos - (start_pos + 1)),
                                        nullptr);
                         obj.texture_indicies.push_back((uint32_t)index - 1);
+                        vert.TextureCoord=textureCoords[index-1];
                     }
                     if (start_pos != end_pos - 1) {
                         index = std::stoul(face.substr(end_pos + 1), nullptr);
                         obj.normal_indicies.push_back((uint32_t)index - 1);
+                        vert.Normal= normals[index-1];
                     }
+                    obj.vertices.push_back(std::move(vert));
                     std::cout << "f " << obj.indicies.back() + 1 << "/"
                               << obj.texture_indicies.back() + 1 << "/"
                               << obj.normal_indicies.back() + 1 << std::endl;
